830ff864c5a16bb03a2eb85abb1239330f0d0e1f
Lucas Felix <lucas.felixandrade@hotmail.com.br>
2023-10-30 16:05:12 -0300

    feat: more methods for characters

--- Controllers/PersonagensController.cs
+++ Controllers/PersonagensController.cs
@@ -40,6 +40,59 @@ namespace RpgApi.Controllers
             }
         }
 
+        [HttpGet("GetByUser/{userId}")]
+        public async Task<IActionResult> GetByUserAsync(int userId)
+        {
+            try
+            {
+                List<Personagem> lista = await _context.TB_PERSONAGENS
+                .Where(u => u.Usuario.Id == userId).ToListAsync();
+
+                return Ok(lista);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("GetByPerfil/{userId}")]
+        public async Task<IActionResult> GetByPerfilAsync(int userId)  {
+            try
+            {
+                Usuario usuario = await _context.TB_USUARIOS
+                .FirstOrDefaultAsync(x => x.Id == userId);
+
+                List<Personagem> lista = new List<Personagem>();
+
+                if (usuario.Perfil == "Admin")
+                    lista = await _context.TB_PERSONAGENS.ToListAsync();  
+                else
+                    lista = await _context.TB_PERSONAGENS
+                        .Where(p => p.Usuario.Id == userId).ToListAsync();  return Ok(lista);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        } 
+
+        [HttpGet("GetByNomeAproximado/{nomePersonagem}")]
+        public async Task<IActionResult> GetByNomeAproximado(string nomePersonagem)  {
+            try
+            {
+                List<Personagem> lista = await _context.TB_PERSONAGENS 
+                    .Where(p => p.Nome.ToLower().Contains(nomePersonagem.ToLower()))
+                    .ToListAsync();
+                    
+                return Ok(lista);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        } 
+
         [HttpGet("GetAll")]
         public async Task<IActionResult> Get()
         {
@@ -97,6 +150,86 @@ namespace RpgApi.Controllers
                 return BadRequest(ex.Message);
             }
         }
+
+        [HttpPut("AtualizarFoto")]
+        public async Task<IActionResult> AtualizarFotoAsync(Personagem p)
+        {
+            try
+            {
+                Personagem personagem = await _context.TB_PERSONAGENS.FirstOrDefaultAsync(x => x.Id == p.Id);
+
+                personagem.FotoPersonagem = p.FotoPersonagem;
+                var attach = _context.Attach(personagem);
+
+                attach.Property(x => x.Id).IsModified = false;
+                attach.Property(x => x.FotoPersonagem).IsModified = true;  int linhasAfetadas = await _context.SaveChangesAsync();  return Ok(linhasAfetadas);
+            }
+            catch (System.Exception ex)
+            { 
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("RestaurarPontosVida")]
+        public async Task<IActionResult> RestaurarPontosVidaAsync(Personagem p)
+        {
+            try
+            {
+                int linhasAfetadas = 0;
+                Personagem pEncontrado = await _context.TB_PERSONAGENS.FirstOrDefaultAsync(pBusca => pBusca.Id == p.Id);  pEncontrado.PontosVida = 100;
+
+                bool atualizou = await TryUpdateModelAsync<Personagem>(pEncontrado, "p",  pAtualizar => pAtualizar.PontosVida);
+
+                linhasAfetadas = await _context.SaveChangesAsync();
+                return Ok(linhasAfetadas);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("ZerarRanking")]
+        public async Task<IActionResult> ZerarRankingAsync(Personagem p)
+        {
+            try
+            {
+                Personagem pEncontrado = await _context.TB_PERSONAGENS.FirstOrDefaultAsync(pBusca => pBusca.Id == p.Id);  pEncontrado.Disputas = 0;
+
+                pEncontrado.Vitorias = 0;
+                pEncontrado.Derrotas = 0;
+                int linhasAfetadas = 0;
+
+                bool atualizou = await TryUpdateModelAsync<Personagem>(pEncontrado, "p",  pAtualizar => pAtualizar.Disputas,
+                pAtualizar => pAtualizar.Vitorias,
+                pAtualizar => pAtualizar.Derrotas);
+
+                linhasAfetadas = await _context.SaveChangesAsync();  return Ok(linhasAfetadas);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message); 
+            } 
+        }
+
+        [HttpPut("ZerarRankingRestaurarVidas")]
+        public async Task<IActionResult> ZerarRankingRestaurarVidasAsync()  {
+            try
+            {
+                List<Personagem> lista =
+                await _context.TB_PERSONAGENS.ToListAsync();
+                foreach (Personagem p in lista)
+                {
+                await ZerarRankingAsync(p);
+                await RestaurarPontosVidaAsync(p);
+                }
+                return Ok();
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        } 
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
